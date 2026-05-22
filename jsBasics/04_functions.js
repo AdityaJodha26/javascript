3977469f015ffe691fc59f0bd8ed5e97390489d9
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-05-22 16:15:02 +0000

    committing

---
+++ jsBasics/04_functions.js
@@ -0,0 +1,15 @@
+//what are functions - these are the reusable code which work when they are called 
+//how it is created  function keyword than function name parenthesis { code} ;
+function multiply(a,b){
+    return a+b  ;
+}
+// call the funtion 
+console.log(multiply(3 , 43)) ; 
+
+// functions input and output 
+// parameter - are the values sent to the function - these are the variables written inside the function definition 
+//arguments are the values received by the function 
+
+// function calling vs referencing - sayhello is the function , sayhello() is calling the function 
+
+// for parameters in js you do not have to define datatype in the function definition 
