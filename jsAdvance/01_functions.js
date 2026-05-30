fae12f88eea42f3bf68c42591a71b289f32a3326
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-05-30 04:53:02 +0000

    commit

---
+++ jsAdvance/01_functions.js
@@ -0,0 +1,14 @@
+// functions are objects  - arguments.length = the number of arguments received by the functions 
+// toString() returns a method as a string
+
+//Callback Function is a function which is passed as an argument into another function
+      // a callback function is intended to be executed later
+      // when you a fn as an argument do not use parenthesis
+      
+//this keyword - when it is used in a function it refers to the object 
+    // functions are object methods
+    // when this is used inside the function this refers to the object that owns that method
+    // in default function this refers to the global object 
+    // in strict it is undefined 
+    // this in event handlers refers to the element
+    // arrow function do not have their own this they inherit from surrounding object 
